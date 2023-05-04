1c83953ab7d3932868f9568d61a6477620ef3cad
AlexssanderMartins <131707308+AlexssanderMartins@users.noreply.github.com>
2023-05-03 23:51:06 -0500

    PLESAE HELP MEEEE

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/entry/error.svelte.f255e92e.js","_app/immutable/chunks/index.7cf8fd15.js","_app/immutable/chunks/singletons.a40828fa.js","_app/immutable/chunks/index.71f4be7d.js"];
-export const stylesheets = [];
-export const fonts = [];
